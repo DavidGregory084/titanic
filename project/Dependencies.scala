c7d49fac242f6e3664df66fe143ffed1fc5e3e6a
David Gregory <davidgregory084@gmail.com>
2017-12-16 15:45:20 +0000

    Update dependency coordinate for schemes release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._, Keys._
 object Dependencies {
   val purecsv = "com.github.melrief" %% "purecsv" % "0.1.1"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
-  val schemes = "io.github.davidgregory084" %% "schemes-core" % "0.1.0-SNAPSHOT"
+  val schemes = "io.github.davidgregory084" %% "schemes-core" % "0.1.0"
 }
