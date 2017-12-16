41e7fa0e0f9ad88cc5229cbdf1d88d636b3ab290
David Gregory <davidgregory084@gmail.com>
2017-12-16 00:14:12 +0000

    Revert Config change

--- src/main/scala/titanic/Config.scala
+++ src/main/scala/titanic/Config.scala
@@ -3,7 +3,7 @@ package titanic
 object Config {
 
   // path to the resources folder, in case of classpath issues.
-  val basePath: String = "/home/david/Repos/titanic/src/main/resources/"
+  val basePath: String = ???
 
   val preshuffledFileName: String = basePath + "original.csv"
   val trainingFileName: String = basePath + "training.csv"
